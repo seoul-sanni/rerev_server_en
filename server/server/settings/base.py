2b588c33444d02d9cb9e251c3db046105bf8fe1a
Seobi Oh <seobioh.biz@gmail.com>
2025-10-30 18:46:09 +0900

    feat(settings): enable django-crontab and add billing cron jobs
    - Add django_crontab to INSTALLED_APPS
    - Schedule subscriptions.cron.perform_billing at 09:00 and 18:00
    - Reminder: register with `python manage.py crontab add && python manage.py crontab show`

--- server/server/settings/base.py
+++ server/server/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',     # JWT
     'corsheaders',                  # CORS
+    'django_crontab',               # Crontab
     'drf_spectacular',              # Swagger
 
     # My apps
